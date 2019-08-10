d11ecf3a742aa93205b24534033d045ff62fcfcc
Cameron May <camerondmay@gmail.com>
2019-08-11 08:52:14 +1200

    AudioSwap

--- slideshow.js
+++ slideshow.js
@@ -5,7 +5,7 @@ function showIntro() {
   image(bg, 0, 0, width, height);
   textSize(lmax * 5);
   textAlign(CENTER);
-  text("Circular Dotscape", width / 2, height * 0.4);
+  text("Ringscape", width / 2, height * 0.4);
   textSize(lmax * 3.5);
   text("Touch the screen to begin", width / 2, height * 0.6);
   if (textStroke < 80) {
